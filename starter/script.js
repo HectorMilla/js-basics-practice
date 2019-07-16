2bea521df8f09738ecc3027b73abdada059c022d
hector milla <hectormilla8@gmail.com>
2019-07-15 22:20:45 -0400

    test commit

---
+++ starter/script.js
@@ -0,0 +1,10 @@
+console.log("hey whats up");
+
+const firstName = "John";
+const lastName = "Smith";
+let age = 21;
+let job = "Teacher";
+
+console.log(
+  `hey my name is ${firstName} ${lastName} I am ${age} years old. I work as a ${job}`
+);
