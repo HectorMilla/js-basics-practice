0f6bd7f64461a6b0ac4fa72642538fcd96594408
hector milla <hectormilla8@gmail.com>
2019-07-16 18:42:54 -0400

    variable mutation and type coercion

--- starter/script.js
+++ starter/script.js
@@ -1,10 +1,25 @@
-console.log("hey whats up");
+// console.log("hey whats up");
 
-const firstName = "John";
-const lastName = "Smith";
-let age = 21;
-let job = "Teacher";
+// const firstName = "John";
+// const lastName = "Smith";
+// let age = 21;
+// let job = "Teacher";
 
+// console.log(
+//   `hey my name is ${firstName} ${lastName} I am ${age} years old. I work as a ${job}`
+// );
+// this is how you comment
+/* multi line comment*/
+// variable mtation and type coercion
+var age = 21;
+var firstName = "john";
+
+console.log(firstName + " " + age);
+var job, isMarried;
+job = "teacher";
+isMarried = false;
+
+age = "twenty eight";
 console.log(
-  `hey my name is ${firstName} ${lastName} I am ${age} years old. I work as a ${job}`
+  `hey my name is ${firstName}  I am ${age} years old. I work as a ${job}`
 );
